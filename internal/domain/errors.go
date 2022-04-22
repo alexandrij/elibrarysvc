d1792cdded919eadc42519c4db72a76fffad71f9
adimitriev <a.dimitriev.86@yandex.ru>
2022-04-22 09:18:05 +0300

    Бизнес ошибки

---
+++ internal/domain/errors.go
@@ -0,0 +1,9 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInconsistentIDs = errors.New("inconsistent IDs")
+	ErrAlreadyExists   = errors.New("already exists")
+	ErrNotFound        = errors.New("not found")
+)
